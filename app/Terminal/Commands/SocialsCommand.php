b0b81a84840b7f0d33f0f64e782c9efc4cff3c7c
Neluxx <fabian.arndt96@proton.me>
2026-02-27 15:26:42 +0100

    feat: add socials command and view with dummy data

---
+++ app/Terminal/Commands/SocialsCommand.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Terminal\Commands;
+
+class SocialsCommand extends AbstractCommand
+{
+    public function getName(): string { return 'socials'; }
+    public function getDescription(): string { return 'Find me on the internet'; }
+
+    public function execute(string $args = ''): string
+    {
+        return $this->view('terminal.responses.socials');
+    }
+}
---
+++ resources/views/terminal/responses/socials.blade.php
@@ -0,0 +1,6 @@
+<div class="space-y-1">
+    <p class="text-ubuntu-yellow font-bold mb-2">Socials</p>
+    <p><span class="text-ubuntu-green">GitHub:</span>   <a href="https://github.com/yourusername" target="_blank" class="underline hover:text-ubuntu-yellow">github.com/yourusername</a></p>
+    <p><span class="text-ubuntu-green">LinkedIn:</span> <a href="https://linkedin.com/in/yourusername" target="_blank" class="underline hover:text-ubuntu-yellow">linkedin.com/in/yourusername</a></p>
+    <p><span class="text-ubuntu-green">Twitter:</span>  <a href="https://twitter.com/yourusername" target="_blank" class="underline hover:text-ubuntu-yellow">twitter.com/yourusername</a></p>
+</div>
