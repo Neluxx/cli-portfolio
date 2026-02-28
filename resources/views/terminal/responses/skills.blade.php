660b16fb6b702d92e351de2398155408fbbf4557
Neluxx <fabian.arndt96@proton.me>
2026-02-28 12:59:39 +0100

    feat: update languages, frameworks and tools in skills view

--- resources/views/terminal/responses/skills.blade.php
+++ resources/views/terminal/responses/skills.blade.php
@@ -1,6 +1,6 @@
 <div class="space-y-1">
-    <p class="text-ubuntu-yellow font-bold mb-2">Skills</p>
-    <p><span class="text-ubuntu-green">Languages:</span> PHP, JavaScript, TypeScript</p>
-    <p><span class="text-ubuntu-green">Frameworks:</span> Laravel, Vue.js, Livewire</p>
-    <p><span class="text-ubuntu-green">Tools:</span> Docker, Git, MySQL, Redis</p>
+    <p class="text-ubuntu-yellow font-bold">Skills</p>
+    <p><span class="text-ubuntu-green">Languages:</span> PHP, Python, Java, JavaScript</p>
+    <p><span class="text-ubuntu-green">Frameworks:</span> Laravel, CakePHP, Symfony, Django</p>
+    <p><span class="text-ubuntu-green">Tools:</span> Docker, Git, MySQL, Apache, Nginx</p>
 </div>
