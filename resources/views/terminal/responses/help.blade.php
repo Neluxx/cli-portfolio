6db4c407d70da8940abdc3cd098b15ca16ae8302
Neluxx <fabian.arndt96@proton.me>
2026-02-28 13:11:02 +0100

    style: update styles in help view

--- resources/views/terminal/responses/help.blade.php
+++ resources/views/terminal/responses/help.blade.php
@@ -1,10 +1,9 @@
-<?php ?>
 <div class="space-y-1">
-    <p class="text-ubuntu-yellow font-bold mb-2">Available commands:</p>
+    <p class="text-ubuntu-yellow font-bold">Available commands:</p>
     @foreach ($commands as $name => $command)
-        <div class="grid grid-cols-[160px_1fr] gap-2">
+        <div class="grid grid-cols-[140px_1fr]">
             <span class="text-ubuntu-cyan">{{ $name }}</span>
-            <span class="text-ubuntu-fg">{{ $command->getDescription() }}</span>
+            <span class="text-ubuntu-white">{{ $command->getDescription() }}</span>
         </div>
     @endforeach
     <p class="text-ubuntu-purple mt-2 text-xs">Tip: use ↑ / ↓ to navigate command history · Ctrl+L to clear</p>
