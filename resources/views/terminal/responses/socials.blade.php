c4eb8a375a1b4671a4bc125dca0c3e604a3eb395
Neluxx <fabian.arndt96@proton.me>
2026-02-28 13:04:04 +0100

    style: update links and styling in socials view

--- resources/views/terminal/responses/socials.blade.php
+++ resources/views/terminal/responses/socials.blade.php
@@ -1,6 +1,5 @@
 <div class="space-y-1">
-    <p class="text-ubuntu-yellow font-bold mb-2">Socials</p>
-    <p><span class="text-ubuntu-green">GitHub:</span>   <a href="https://github.com/yourusername" target="_blank" class="underline hover:text-ubuntu-yellow">github.com/yourusername</a></p>
-    <p><span class="text-ubuntu-green">LinkedIn:</span> <a href="https://linkedin.com/in/yourusername" target="_blank" class="underline hover:text-ubuntu-yellow">linkedin.com/in/yourusername</a></p>
-    <p><span class="text-ubuntu-green">Twitter:</span>  <a href="https://twitter.com/yourusername" target="_blank" class="underline hover:text-ubuntu-yellow">twitter.com/yourusername</a></p>
+    <p class="text-ubuntu-yellow font-bold">Socials</p>
+    <p><span class="text-ubuntu-green">GitHub: </span><a href="https://github.com/neluxx" target="_blank" class="text-ubuntu-blue hover:underline">github.com/neluxx</a></p>
+    <p><span class="text-ubuntu-green">LinkedIn: </span><a href="https://linkedin.com/in/arndt-fabian" target="_blank" class="text-ubuntu-blue hover:underline">linkedin.com/in/arndt-fabian</a></p>
 </div>
