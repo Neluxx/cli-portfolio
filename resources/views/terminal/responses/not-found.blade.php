0377abccd79c4d96aafff2f5551c859b5b0613bc
Neluxx <fabian.arndt96@proton.me>
2026-02-28 13:05:43 +0100

    style: update wording and colors in not-found view

--- resources/views/terminal/responses/not-found.blade.php
+++ resources/views/terminal/responses/not-found.blade.php
@@ -1,5 +1,4 @@
-<?php ?>
 <div class="text-ubuntu-red">
-    Command '<span class="font-bold">{{ $command }}</span>' not found!
-    <span class="text-ubuntu-gray ml-2">— try <span class="text-ubuntu-yellow">help</span> to list available commands</span>
+    <span class="font-bold">{{ $command }}</span>: command not found
+    <span class="text-ubuntu-purple">— try <span class="text-ubuntu-yellow">help</span> to list available commands</span>
 </div>
