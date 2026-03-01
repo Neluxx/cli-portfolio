d87821c11c250be235f480078531b2addfe9c480
Neluxx <fabian.arndt96@proton.me>
2026-03-01 12:46:22 +0100

    style: update text and layout in about view

--- resources/views/terminal/responses/about.blade.php
+++ resources/views/terminal/responses/about.blade.php
@@ -1,5 +1,6 @@
 <div class="space-y-1">
     <p class="text-ubuntu-yellow font-bold mb-2">About Me</p>
-    <p>Hi! I'm a software developer passionate about building great web experiences.</p>
+    <p class="mb-4">Hey, my name is Fabian. I am a full-stack software developer from Germany.</p>
+    <p class="mb-4">I began my career with an apprenticeship in media design, which introduced me to web development and motivated me to pursue a degree in computer science. Since graduating, I have focused on software development, driven by curiosity and a commitment to continuously expand my knowledge of emerging technologies.</p>
     <p>I love clean code, open-source, and a good cup of coffee ☕</p>
 </div>
