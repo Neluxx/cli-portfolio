596d523895de17d8aa9cd2da927672913434849f
Neluxx <fabian.arndt96@proton.me>
2026-03-01 12:43:57 +0100

    refactor: remove unused font links in terminal layout

--- resources/views/layouts/terminal.blade.php
+++ resources/views/layouts/terminal.blade.php
@@ -1,15 +1,11 @@
-<?php ?>
 <!DOCTYPE html>
 <html lang="de">
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>~/portfolio $</title>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>~/portfolio $</title>
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @livewireStyles
-<link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-<link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@300;400;500;700&display=swap" rel="stylesheet" />
 </head>
 <body class="bg-chrome-body min-h-screen flex items-start justify-center p-4 md:p-8">
 
